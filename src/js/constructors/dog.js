9eabb7c7e2c48e6dffac84b63d5937c3f9f4f732
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-21 17:50:26 -0500

    javascript finished

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,10 @@
-function Dog () {
-}
+function Dog (info) {
+  info = info || {};
+  this.color = info.color;
+  this.hungry = (info.hungry == undefined ? true : false);
+  this.status = info.status || "normal";
+  this.owner = info.owner;
+  }
+
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,14 @@
-function Person () {
+function Person (people) {
+  people = people || {};
+  this.cool = (people.cool == undefined ? false : true);
+}
+
+Person.prototype.pet = function (petted) {
+  petted.status = "happy";
 };
+
+Person.prototype.feed = function (feeding) {
+  feeding.hungry = false;
+}
 
 export { Person };
